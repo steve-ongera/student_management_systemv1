cfdf12c30848623647368297e74e013db933b31f
steve-ongera <gadafisteve001@gmail.com>
2026-06-02 00:30:54 +0300

    made updates

---
+++ backend/src/routes/studentRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getAllStudents,
+  getStudentById,
+  createStudent,
+  updateStudent,
+  deleteStudent
+} = require('../controllers/studentController');
+
+router.get('/', getAllStudents);
+router.get('/:id', getStudentById);
+router.post('/', createStudent);
+router.put('/:id', updateStudent);
+router.delete('/:id', deleteStudent);
+
+module.exports = router;
